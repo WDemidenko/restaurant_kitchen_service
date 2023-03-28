1e53ce0ea83ac4b45d43758604e54f522034ae00
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-03-28 23:05:51 +0200

    Add index view with counts of Cooks DishTypes and Dishes

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Dish, DishType, Cook
+
+
+def index(request):
+    num_cooks = Cook.objects.count()
+    num_dishtypes = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishtypes": num_dishtypes,
+        "num_dishes": num_dishes,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
